7477c891ccca2a78669a3bfafef7be4a7ff4db30
Ranu <O@O.net>
2018-11-21 15:07:35 +0000

    Hello world style home component

--- web/AngularClient/src/app/home/home.component.ts
+++ web/AngularClient/src/app/home/home.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  public homeText: string;
+
   constructor() { }
 
   ngOnInit() {
+    this.homeText = "WELCOME TO Penguin APPLICATION";
   }
 
 }
