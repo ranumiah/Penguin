f647b70d8800e340d108cd1e898a68a09cc6ce58
Ranu <O@O.net>
2018-11-28 21:38:13 +0000

    Add child/parent relation for owner and accounts

--- web/AngularClient/src/app/owner/owner.module.ts
+++ web/AngularClient/src/app/owner/owner.module.ts
@@ -1,11 +1,11 @@
+import { SharedModule } from './../shared/shared.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 import { OwnerListComponent } from './owner-list/owner-list.component';
 import { OwnerDetailsComponent } from './owner-details/owner-details.component';
-
-import { SharedModule } from '../shared/shared.module';
+import { OwnersaccountsComponent } from './owner-details/ownersaccounts/ownersaccounts.component';
 
 @NgModule({
   imports: [
@@ -18,7 +18,8 @@ import { SharedModule } from '../shared/shared.module';
   ],
   declarations: [
     OwnerListComponent,
-    OwnerDetailsComponent
+    OwnerDetailsComponent,
+    OwnersaccountsComponent
   ]
 })
 export class OwnerModule { }
--- web/AngularClient/src/app/shared/shared.module.ts
+++ web/AngularClient/src/app/shared/shared.module.ts
@@ -2,18 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ErrorModalComponent } from './modals/error-modal/error-modal.component';
 import { SuccessModalComponent } from './modals/success-modal/success-modal.component';
+import { DatepickerDirective } from './directives/datapicker.directive';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: [
-        ErrorModalComponent,
-        SuccessModalComponent
-    ],
-    exports: [
-        ErrorModalComponent,
-        SuccessModalComponent
+  ErrorModalComponent,
+  SuccessModalComponent,
+  DatepickerDirective
+],
+  exports: [
+      ErrorModalComponent,
+      SuccessModalComponent,
+      DatepickerDirective
   ]
 })
 export class SharedModule { }
