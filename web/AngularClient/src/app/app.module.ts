70216c15c804a827316cd06775d1ca9ff8d79db3
Ranu <O@O.net>
2018-11-28 12:45:16 +0000

    Add repository service for consuming api server

--- web/AngularClient/src/app/app.module.ts
+++ web/AngularClient/src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { MenuComponent } from './menu/menu.component';
 import { NotFoundComponent } from './error-pages/not-found/not-found.component';
 
-
 import { EnvironmentUrlService } from './shared/services/environment-url.service';
+import { RepositoryService } from './shared/services/repository.service';
 
 @NgModule({
   declarations: [
@@ -19,6 +20,7 @@ import { EnvironmentUrlService } from './shared/services/environment-url.service
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     RouterModule.forRoot([
       { path: 'home', component: HomeComponent },
       { path: '404', component : NotFoundComponent},
@@ -26,7 +28,10 @@ import { EnvironmentUrlService } from './shared/services/environment-url.service
       { path: '**', redirectTo: '/404', pathMatch: 'full'}
     ])
   ],
-  providers: [EnvironmentUrlService],
+  providers: [
+    EnvironmentUrlService,
+    RepositoryService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
