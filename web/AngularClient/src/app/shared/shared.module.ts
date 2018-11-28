568ce55790d59d005745e0967f226e119079c69c
Ranu <O@O.net>
2018-11-28 18:04:58 +0000

    ng g component shared/modals/error-modal ==> create error modal component

--- web/AngularClient/src/app/shared/shared.module.ts
+++ web/AngularClient/src/app/shared/shared.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ErrorModalComponent } from './modals/error-modal/error-modal.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: [
-  ],
-  exports: []
+        ErrorModalComponent
+    ],
+    exports: [
+        ErrorModalComponent
+  ]
 })
 export class SharedModule { }
