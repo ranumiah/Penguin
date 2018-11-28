fd0a813f6e1a098254e8354b9705603eefdd4980
Ranu <O@O.net>
2018-11-28 16:06:55 +0000

    Interface, Subscription and Data Display

--- web/AngularClient/src/app/owner/owner-list/owner-list.component.ts
+++ web/AngularClient/src/app/owner/owner-list/owner-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { RepositoryService } from './../../shared/services/repository.service';
+import { Owner } from './../../_interfaces/owner.model';
 
 @Component({
   selector: 'app-owner-list',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./owner-list.component.css']
 })
 export class OwnerListComponent implements OnInit {
+  public owners: Owner[];
 
-  constructor() { }
+  constructor(private repository: RepositoryService) { }
 
   ngOnInit() {
+    this.getAllOwners();
   }
 
+  public getAllOwners(){
+    let apiAddress: string = "api/owner";
+    this.repository.getData(apiAddress)
+    .subscribe(res => {
+      this.owners = res as Owner[];
+    })
+  }
 }
